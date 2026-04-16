00f42753fa977b3b53072cd95754286a4b22bf44
unknown <danielrillera2@gmail.com>
2026-04-16 15:09:32 +0800

    Enable display_errors for debugging

--- employee/update_days_worked.php
+++ employee/update_days_worked.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/update_days_worked_errors.log');
 error_log("[update_days_worked.php] ========== SCRIPT STARTED ==========");
